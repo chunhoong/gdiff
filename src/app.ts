a4123a681d5bb9ec4cb29830098477bf71ce8ec6
Lim Chun Hoong <chunhoong.lim@outlook.my>
2021-04-09 12:48:59 +0800

    Fix naming error during refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import { exec as app } from 'shelljs';
+import { exec } from 'shelljs';
 
 export const commitIdOfLatestTag = () => {
-  const tagName = app('git describe --tags --abbrev=0', { silent: true });
-  return app(`git rev-list -n 1 ${tagName}`, { silent: true }); 
+  const tagName = exec('git describe --tags --abbrev=0', { silent: true });
+  return exec(`git rev-list -n 1 ${tagName}`, { silent: true }); 
 }
 
 export const commitIdOfLatestCommit = () => {
-  return app(`git rev-parse HEAD`, { silent: true });
+  return exec(`git rev-parse HEAD`, { silent: true });
 }
 
 export const changedFiles = () => {
-  const output = app(`git diff --name-only ${commitIdOfLatestTag()} ${commitIdOfLatestCommit()}`, { silent: true });
+  const output = exec(`git diff --name-only ${commitIdOfLatestTag()} ${commitIdOfLatestCommit()}`, { silent: true });
   const changeFiles = output.split('\n');
   changeFiles.pop();
   return changeFiles;
