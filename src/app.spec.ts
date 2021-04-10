e2aba6311388cc1c2bc8566a4818e232a2a0ad1e
Lim Chun Hoong <chunhoong.lim@outlook.my>
2021-04-10 14:38:03 +0800

    Improve test

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -2,9 +2,9 @@ import * as app from './app';
 import { ERROR_MESSAGE_COMMIT_IDENTICAL } from './app';
 
 it('should return list of changed files between 2 commits', () => {
-  const commitIdOfLatestTag = '856492b367f132a05a50c16f1d98d48177fb1c55';
-  const commitIdOfLatestCommit = '6ea8b5f303d09abd6f133e4854a8d185e2555d60';
-  const changedFiles = app.changedFiles(commitIdOfLatestTag, commitIdOfLatestCommit);
+  const commit1 = '856492b367f132a05a50c16f1d98d48177fb1c55';
+  const commit2 = '6ea8b5f303d09abd6f133e4854a8d185e2555d60';
+  const changedFiles = app.changedFiles(commit1, commit2);
   expect(changedFiles).toBeDefined();
   expect(changedFiles.length).toEqual(7);
 });
